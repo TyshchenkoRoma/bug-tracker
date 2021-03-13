d33811efe6da3d0c262071580f0bef9bb686d031
Roman Tyshchenko <rtyshchenko@griddynemics.com>
2021-03-13 18:34:12 +0200

    hw 4 finish

---
+++ src/main/java/com/example/demo/entity/User.java
@@ -0,0 +1,21 @@
+package com.example.demo.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.RequiredArgsConstructor;
+
+@Data
+@RequiredArgsConstructor
+@AllArgsConstructor
+public class User {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    public User(String firstName, String lastName, String email) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+}
