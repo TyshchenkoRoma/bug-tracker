270aa53372884033ca9aa7f0900f0f3bd4b6a2fd
Roman Tyshchenko <rtyshchenko@griddynemics.com>
2021-04-08 07:59:30 +0300

    HW7

--- src/main/java/com/example/demo/HW7.java
+++ src/main/java/com/example/demo/HW7.java
@@ -5,9 +5,11 @@ import org.springframework.beans.factory.config.BeanPostProcessor;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+
 class HW7 implements BeanPostProcessor {
 
     @Override
+
     public Object postProcessBeforeInitialization(Object bean, String beanName) throws BeansException {
         System.out.println("Called postProcessBeforeInitialization() for: " + beanName);
 
