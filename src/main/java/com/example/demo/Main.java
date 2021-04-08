aeb3bfcaa47d6c00fe8e8898750ec5dc069eeade
Roman Tyshchenko <rtyshchenko@griddynemics.com>
2021-04-08 07:54:38 +0300

    HW7

--- src/main/java/com/example/demo/Main.java
+++ src/main/java/com/example/demo/Main.java
@@ -14,15 +14,19 @@ public class Main {
     @Autowired
     AbstractRespository abstractRespository;
 
+    @Autowired
+    HW7 hw7;
+
     public static void main(String[] args) {
         SpringApplication.run(Main.class, args);
     }
 
     @PostConstruct
     public void exequteQuerry() {
+        System.out.println("STARTING APP");
         abstractRespository.create(new User("roma", "tysh", "1@1.com", new Role("Role name 1 ")));
-        abstractRespository.create(new User( "roma2", "tysh2", "2@1.com", new Role("Role name 2 ")));
-        abstractRespository.create(new User( "roma3", "tysh3", "3@1.com", new Role("Role name 3 ")));
+        abstractRespository.create(new User("roma2", "tysh2", "2@1.com", new Role("Role name 2 ")));
+        abstractRespository.create(new User("roma3", "tysh3", "3@1.com", new Role("Role name 3 ")));
         abstractRespository.getAll().stream().forEach(System.out::println);
     }
 }
